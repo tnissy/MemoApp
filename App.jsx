2e6cb8458b16210b6caa6dccae6acf080e594284
tnissy <30714064+tnissy@users.noreply.github.com>
2021-03-08 19:42:46 +0900

    hello component :wq

--- App.jsx
+++ App.jsx
@@ -1,11 +1,17 @@
+/* eslint-disable no-use-before-define */
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Hello from './src/components/Hello';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>Noi</Hello>
+      <Hello bang style={{ fontSize: 16 }}>Noita</Hello>
       <Text>Open up App.js to start working on your app!</Text>
+      {/* eslint-disable-next-line react/style-prop-object */}
       <StatusBar style="auto" />
     </View>
   );
