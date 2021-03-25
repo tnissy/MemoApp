58bc3bf5d93038b7fab9b23dc050f438e6839d4e
tnissy <30714064+tnissy@users.noreply.github.com>
2021-03-25 22:01:15 +0900

    vscodeから

--- App.jsx
+++ App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
+import firebase from 'firebase';
 
 import LogInScreen from './src/screens/LogInScreen';
 import MemoListScreen from './src/screens/MemoListScreen';
@@ -11,11 +12,23 @@ import SignUpScreen from './src/screens/SignUpScreen';
 
 const Stack = createStackNavigator();
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyDf_LIJXW-JbVDFj--k7sH17IN3h_9gRI8',
+  authDomain: 'memoapp-82241.firebaseapp.com',
+  projectId: 'memoapp-82241',
+  storageBucket: 'memoapp-82241.appspot.com',
+  messagingSenderId: '408397951302',
+  appId: '1:408397951302:web:68ef63f538c613fc233547',
+};
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="LogIn"
+        initialRouteName="SignUp"
         screenOptions={{
           headerStyle: { backgroundColor: '#467fd3' },
           headerTitleStyle: { color: '#ffffff' },
